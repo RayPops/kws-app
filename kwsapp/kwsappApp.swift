5c96612a36e9c0fe705ed03c03709b3f59c11a86
Rayan <rayan.popat@kcl.ac.uk>
2023-06-20 18:01:58 +0300

    Initial Commit

---
+++ kwsapp/kwsappApp.swift
@@ -0,0 +1,17 @@
+//
+//  kwsappApp.swift
+//  kwsapp
+//
+//  Created by Rayan Popat on 20/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct kwsappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
