202726656d49b69963b00e07716fb6b2788048e0
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2023-08-21 20:18:08 +0300

    fixed loading issues
    fixed loading issues

--- kwsapp/Views/HomeView.swift
+++ kwsapp/Views/HomeView.swift
@@ -10,54 +10,50 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         VStack {
-            ZStack {
-                Color.black
-                    .frame(height: 50)
-                    .edgesIgnoringSafeArea(.top)
-
-                HStack {
-                    Image("logo")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(height: 40)
-                        .padding(.leading)
-
-                    Spacer()
-
-                    Button(action: {
-                        // Search action
-                    }) {
-                        Image(systemName: "magnifyingglass")
-                            .foregroundColor(Color("TabButtonAccentColor"))
-                    }
-
-                    Button(action: {
-                        // Account action
-                    }) {
-                        Image(systemName: "person")
-                            .foregroundColor(Color("TabButtonAccentColor"))
-                    }
-                    .padding(.trailing)
+            
+            HStack {
+                Image("logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 40)
+                    .padding(.leading)
+                
+                Spacer()
+                
+                Button(action: {
+                    // Search action
+                }) {
+                    Image(systemName: "magnifyingglass")
+                        .foregroundColor(Color("TabButtonAccentColor"))
+                }
+                
+                Button(action: {
+                    // Account action
+                }) {
+                    Image(systemName: "person")
+                        .foregroundColor(Color("TabButtonAccentColor"))
+                }
+                .padding(.trailing)
+            }
+            
+            
+            GeometryReader { geometry in
+                ZStack {
+                    ImageCarouselView()
+                        .padding(.top, 5.0)
+                        .frame(height: geometry.size.height * 1)
+                    
+                    Text("Welcome to Kenya")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
                 }
             }
-
-            GeometryReader { geometry in
-            ZStack {
-                ImageCarouselView()
-                    .padding(.top, 5.0)
-                        .frame(height: geometry.size.height * 1)
-
-                    Text("Welcome to Kenya")
-                         .font(.largeTitle)
-                         .fontWeight(.bold)
-                         .foregroundColor(.white)
-                 }
-             }
-
-             Spacer()
-         }
-     }
- }
+            
+            Spacer()
+        }
+    }
+}
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
